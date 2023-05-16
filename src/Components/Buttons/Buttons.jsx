45372f21929f600a066916afdd224b976470a7ca
TornikeL38 <120584873+TornikeL38@users.noreply.github.com>
2023-05-16 16:36:29 +0400

    change

--- src/Components/Buttons/Buttons.jsx
+++ src/Components/Buttons/Buttons.jsx
@@ -5,14 +5,14 @@ export default function Buttons(){
 
     const [count, setCount] = useState(0);
 
-    const increase = () => {setCount(count + 1)} 
-    const dicrease = () => {setCount(count - 1)}
+    const calculation = (x) => {setCount(count + x)} 
+    
 
     return(
         <div className="conteiner">
-            <button onClick={() => dicrease(count - 1)} className="minus">-</button>
+            <button onClick={() => calculation(- 1)} className="minus">-</button>
             <div className="countWrapper">I was clicked {count} times</div>
-            <button onClick={() => increase(count + 1)} className="plus">+</button>
+            <button onClick={() => calculation(+ 1)} className="plus">+</button>
         </div>
     )
 }
